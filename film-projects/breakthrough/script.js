700991a00e73e89af6d610b3502dc210bc888d38
lakeisha <sharl281@newschool.edu>
2024-11-13 01:16:08 -0500

    fetch("/chandnisportfolio/film.json")

--- film-projects/breakthrough/script.js
+++ film-projects/breakthrough/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const targetTitle = "Breakthrough - PRIADHS ft. OVRDZE"; // Change this to match the desired title
 
   // Fetch the JSON data from an external file
-  fetch("/../film.json")
+  fetch("/chandnisportfolio/film.json")
     .then((response) => response.json()) // Parse the JSON from the response
     .then((jsonData) => {
       // Find the specific entry where the title matches the targetTitle
